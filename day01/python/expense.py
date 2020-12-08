0a0222ef821286dceab3380239c1e5f4cc7469eb
kittydoor <me@kitty.sh>
2020-12-08 22:21:23 +0100

    Solution for Day 1 Part 2 in Python

--- day01/python/expense.py
+++ day01/python/expense.py
@@ -1,30 +1,39 @@
+import sys
 import itertools
+from functools import reduce
 
-def calculate(values: list[int]) -> int:
+def calculate(values: list[int], count: int) -> int:
     """
     Find the two pairs that add up to 2020 in a list of numbers,
     return their product.
     """
-    if len(values) < 2:
+    if len(values) < count:
         return None
 
     for i in values:
         if i < 0:
             raise ValueError("Expense report contains negative number")
 
-    for first, second in itertools.combinations(values, 2):
-        if first + second == 2020:
-            return first * second
+    for combination in itertools.combinations(values, count):
+        if sum(combination) == 2020:
+            return reduce((lambda x, y: x * y), combination)
 
     # final resort
     return None
 
 if __name__ == '__main__':
-    with open("../input.txt") as f:
+    if len(sys.argv) < 2:
+        print("usage: expense.py <target> <count>")
+        exit(1)
+
+    target = sys.argv[1]
+    count = int(sys.argv[2])
+
+    with open(target) as f:
         contents = f.readlines()
 
     expense_report = [ int(line.strip()) for line in contents ]
 
-    result = calculate(expense_report)
+    result = calculate(expense_report, count)
 
     print(result)
--- day01/python/expense_test.py
+++ day01/python/expense_test.py
@@ -4,27 +4,33 @@ from expense import calculate
 
 class TestExpenseMethod(unittest.TestCase):
     def test_empty(self):
-        self.assertEqual(calculate([]), None)
+        self.assertEqual(calculate([], 2), None)
 
     def test_single(self):
-        self.assertEqual(calculate([1]), None)
+        self.assertEqual(calculate([1], 2), None)
 
     def test_two_2020(self):
-        self.assertEqual(calculate([1000, 1020]), 1000 * 1020)
-        self.assertEqual(calculate([800, 1220]), 800 * 1220)
+        self.assertEqual(calculate([1000, 1020], 2), 1000 * 1020)
+        self.assertEqual(calculate([800, 1220], 2), 800 * 1220)
 
     def test_two_not_2020(self):
-        self.assertEqual(calculate([1000, 1000]), None)
+        self.assertEqual(calculate([1000, 1000], 2), None)
 
     def test_three_2020(self):
-        self.assertEqual(calculate([1000, 1100, 1020]), 1000 * 1020)
+        self.assertEqual(calculate([1000, 1100, 1020], 2), 1000 * 1020)
 
     def test_three_self_product_2020(self):
-        self.assertEqual(calculate([1010, 1000, 1100]), None)
+        self.assertEqual(calculate([1010, 1000, 1100], 2), None)
 
     def test_negative(self):
         with self.assertRaises(ValueError):
-            calculate([-1000, 3020, 500, 413, 645])
+            calculate([-1000, 3020, 500, 413, 645], 2)
+
+    def test_p2_two(self):
+        self.assertEqual(calculate([1000, 1100], 3), None)
+
+    def test_p2_three_2020(self):
+        self.assertEqual(calculate([800, 1200, 20], 3), 800 * 1200 * 20)
 
 if __name__ == '__main__':
     unittest.main()
